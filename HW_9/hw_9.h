1e31a78df5cc17c21a266509d30d3dce70722562
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-12-01 00:05:14 +0400

    readding hw_9

--- HW_9/hw_9.h
+++ HW_9/hw_9.h
@@ -39,19 +39,3 @@ private:
     std::list<T*> freeList;
 };
 
-
-int main() {
-    const std::size_t size = 10;
-    int buffer[size];
-
-    FreeListAllocator<int> allocator(buffer, size);
-
-    int* p = allocator.allocate(1);
-    *p = 42;
-
-    std::cout << "Allocated integer: " << *p << std::endl;
-
-    allocator.deallocate(p, 1);
-
-    return 0;
-}
